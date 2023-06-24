45de558d41fe0b9c09d2b84fbbc22455fa92e4fb
Jorge R <jorge.retamal@zweicom.com>
2023-06-24 01:26:56 -0400

    feat: Se implementa toggle de completado

--- src/app/todos/todo.actions.ts
+++ src/app/todos/todo.actions.ts
@@ -1,3 +1,4 @@
 import { createAction, props } from '@ngrx/store';
 
-export const crear = createAction('[TODO] Crea todo',props<{texto:string}>());
+export const crear = createAction('[TODO] Crea Todo',props<{texto:string}>());
+export const toggle = createAction('[TODO] Toggle todo',props<{id:number}>());
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { crear } from './todo.actions';
+import { crear, toggle } from './todo.actions';
 import { Todo } from './models/todo.model';
 
 export const estadoInicial :Todo[]=[];
@@ -7,4 +7,15 @@ export const estadoInicial :Todo[]=[];
 export const todoReducer = createReducer(
     estadoInicial,
   on(crear, (state,{texto}) => [...state,new Todo(texto)]),
+  on(toggle, (state,{id}) => {
+    return state.map(todo=>{
+        if(todo.id===id){
+            return{
+                ...todo,
+                completado:!todo.completado
+            }
+        }
+        return todo
+    })
+  }),
 );
