43c976385cb0ee0735d073b451e838de9eef661b
Jorge R <jorge.retamal@zweicom.com>
2023-06-24 02:39:33 -0400

    feat: Se implementan los filtros

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Todo } from '../models/todo.model';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
+import { filtrosValidos } from 'src/app/filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent implements OnInit {
- todos:Todo[] = []
+export class TodoListComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription();
 
- constructor(private store:Store<AppState>){}
+  todos: Todo[] = [];
 
- ngOnInit(): void {
-   this.store.select('todos').subscribe(todos=>this.todos = todos)
- }
+  filtroActual: filtrosValidos;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.store.subscribe((state) => {
+        this.todos = state.todos;
+        this.filtroActual = state.filtro.filtro;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
